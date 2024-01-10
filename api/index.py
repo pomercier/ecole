60774444180b9f49df00e720bcc13e6a1d1d9d06
pomercier <52725516+pomercier@users.noreply.github.com>
2024-01-10 15:34:04 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,19 @@
-import requests
-import json
+from flask import Flask, render_template
 import pandas as pd
 
-url = "https://www.tvanouvelles.ca/api/schools/closed"
-response = requests.get(url)
-data = response.json()["result"]["institutions"]
+app = Flask(__name__)
 
-df = pd.json_normalize(data)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-df
+@app.route('/')
+def index():
+    url = "https://www.tvanouvelles.ca/api/schools/closed"
+    response = requests.get(url)
+    data = response.json()["result"]["institutions"]
+    df = pd.json_normalize(data)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    return df.to_html(header="true", table_id="table")
+
+if __name__ == '__main__':
+    app.run()
